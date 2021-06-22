6ac4bc6e619c698688be92b328ba51111ebd386b
Mahdi <mahdikaseatashin@gmail.com>
2021-06-23 03:28:57 +0430

    Firebase Test Notification implemented!

--- app/src/main/java/com/mahdikaseatashin/pushnotification/MainActivity.kt
+++ app/src/main/java/com/mahdikaseatashin/pushnotification/MainActivity.kt
@@ -2,6 +2,10 @@ package com.mahdikaseatashin.pushnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
