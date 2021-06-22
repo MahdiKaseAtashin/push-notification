2ba6b02f2348fb4ceac219e1e40ab033a0003805
Mahdi <mahdikaseatashin@gmail.com>
2021-06-23 02:38:27 +0430

    Pushe, Push Notification Implemented!

--- app/src/main/java/com/mahdikaseatashin/pushnotification/MainActivity.kt
+++ app/src/main/java/com/mahdikaseatashin/pushnotification/MainActivity.kt
@@ -2,10 +2,12 @@ package com.mahdikaseatashin.pushnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.ronash.pushe.Pushe
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Pushe.initialize(this,true)
     }
 }
